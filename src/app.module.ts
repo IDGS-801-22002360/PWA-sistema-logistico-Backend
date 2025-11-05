571412e1cb109846de888f977fd77f9b0e675d11
marko <81442@alumnos.utleon.edu.mx>
2025-11-05 00:39:26 -0600

    cambios Menores

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
+import { PaisesModule } from './paises/paises.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { AgentesModule } from './agentes/agentes.module';
-import { PaisesModule } from './paises/paises.module';
-import { UsuarioController } from './usuario/usuario.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuariosModule, ProveedoresModule, ClientesModule, AgentesModule, PaisesModule],
-  controllers: [AppController, UsuarioController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'usuarios',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UsuariosModule,
+    ProveedoresModule,
+    ClientesModule,
+    AgentesModule,
+    PaisesModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
