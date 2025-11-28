44cc8bff093e3bbaad08d80150273b081b864c01
IDGS-801-22002360 <81442@alumnos.utleon.edu.mx>
2025-11-28 00:05:54 -0800

    Correccion Solicitudes

--- src/solicitudes/update-solicitud.dto.ts
+++ src/solicitudes/update-solicitud.dto.ts
@@ -1,4 +1,40 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSolicitudDto } from './create-solicitud.dto';
+import { IsInt, IsEnum, IsOptional, IsDateString, IsString, IsNumberString } from 'class-validator';
+import { TipoServicio, TipoCarga } from '../common/enums/transport.enums';
 
-export class UpdateSolicitudDto extends PartialType(CreateSolicitudDto) {}
+export class UpdateSolicitudDto {
+  @IsOptional()
+  @IsInt()
+  id_cliente?: number;
+
+  @IsOptional()
+  @IsEnum(TipoServicio)
+  tipo_servicio?: TipoServicio;
+
+  @IsOptional()
+  @IsEnum(TipoCarga)
+  tipo_carga?: TipoCarga;
+
+  @IsOptional()
+  @IsInt()
+  id_origen_localizacion?: number;
+
+  @IsOptional()
+  @IsInt()
+  id_destino_localizacion?: number;
+
+  @IsOptional()
+  @IsDateString()
+  fecha_solicitud?: string;
+
+  @IsOptional()
+  @IsString()
+  descripcion_mercancia?: string;
+
+  @IsOptional()
+  @IsNumberString()
+  valor_estimado_mercancia?: string;
+
+  @IsOptional()
+  @IsEnum(['nueva', 'en_proceso', 'cotizada', 'rechazada'])
+  estatus?: string;
+}
--- src/usuarios/update-usuario.dto.ts
+++ src/usuarios/update-usuario.dto.ts
@@ -1,10 +1,28 @@
 import { RolUsuario } from './usuario.enum';
+import { IsString, IsEmail, IsEnum, IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateUsuarioDto {
+  @IsOptional()
+  @IsString()
   nombre?: string;
+
+  @IsOptional()
+  @IsString()
   apellido?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
   password?: string;
+
+  @IsOptional()
+  @IsEnum(RolUsuario)
   rol?: RolUsuario;
+
+  @IsOptional()
+  @IsBoolean()
   activo?: boolean;
 }
